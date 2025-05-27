a425a2354622dc55b6b6f3bd3e82780c0fd5a37b
Claudio123509 <claudio.junior29@escola.pr.gov.br>
2025-05-27 08:02:35 -0300

    e

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação 2',
+    'O que é o CSS?',
+    'O CSS é uma linguagem de estilização'
+)
+
+criaCartao(
+    'Programação 3',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Programação 4',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
